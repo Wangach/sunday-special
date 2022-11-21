3168a9d484fd697b271709ae2fec957610637630
John Kimani <jwangach@gmail.com>
2022-11-21 10:58:11 +0300

    Undefined Functions fix

--- js/reus.js
+++ js/reus.js
@@ -9,4 +9,51 @@ let viewMatch = (id) => {
     })
 }
 
-export { viewMatch };
+let payUp = (id) => {
+      Swal.fire({
+        title: 'Are you sure?',
+        text: `Pay Debt With Id ${id}?`,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+        let url = `./api/master.php?a=payIndebt&debId=${id}`;
+        fetch(url)
+        .then(response => response.text())
+        .then((data) => {
+            if(data.indexOf('Paid') > 0){
+                Swal.fire(
+                    'Paid!',
+                    data,
+                    'success'
+                  )
+            }else{
+                Swal.fire(
+                    'Failed!',
+                    data,
+                    'error'
+                  )
+            }
+        })
+        }else{
+            Swal.fire(
+                'Cancelled',
+                'Cancelled :)',
+                'error'
+              )
+        }
+      })
+}
+
+let doneDeal = (id, dte) => {
+  Swal.fire(
+    'Already Paid',
+    `Debt ${id} Was Paid On: ${dte}`,
+    'success'
+  )
+}
+
+export { viewMatch, payUp, doneDeal };
--- js/search.js
+++ js/search.js
@@ -1,5 +1,7 @@
-import { viewMatch } from './reus.js';
+import { viewMatch, payUp, doneDeal } from './reus.js';
 window.viewMatch = viewMatch;
+window.payUp = payUp;
+window.doneDeal =doneDeal;
 //On Load
 window.addEventListener("load", () => {
   const loader = document.querySelector(".loader");
@@ -156,49 +158,3 @@ searchUserForm.addEventListener("submit", (event) => {
 });
 
 
-let payUp = (id) => {
-      Swal.fire({
-        title: 'Are you sure?',
-        text: `Pay Debt With Id ${id}?`,
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes!'
-      }).then((result) => {
-        if (result.isConfirmed) {
-        let url = `./api/master.php?a=payIndebt&debId=${id}`;
-        fetch(url)
-        .then(response => response.text())
-        .then((data) => {
-            if(data.indexOf('Paid') > 0){
-                Swal.fire(
-                    'Paid!',
-                    data,
-                    'success'
-                  )
-            }else{
-                Swal.fire(
-                    'Failed!',
-                    data,
-                    'error'
-                  )
-            }
-        })
-        }else{
-            Swal.fire(
-                'Cancelled',
-                'Cancelled :)',
-                'error'
-              )
-        }
-      })
-}
-
-let doneDeal = (id, dte) => {
-  Swal.fire(
-    'Already Paid',
-    `Debt ${id} Was Paid On: ${dte}`,
-    'success'
-  )
-}
