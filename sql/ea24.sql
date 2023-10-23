86c6bd75115ff77040e44712586d91902de8be20
John Kimani <jwangach@gmail.com>
2023-10-23 06:59:38 +0300

    sql-change

--- sql/ea24.sql
+++ sql/ea24.sql
@@ -7,7 +7,7 @@ CREATE TABLE `outset`.`fairpay_data` ( `id` INT NOT NULL AUTO_INCREMENT , `Hteam
 /*Transactions table*/
 CREATE TABLE `outset`.`transactions` ( `id` INT NOT NULL AUTO_INCREMENT , `trName` VARCHAR(100) NOT NULL , `trId` VARCHAR(100) NOT NULL , `credit` INT NOT NULL , `debit` INT NOT NULL , `amount` INT NOT NULL , `pmeth` VARCHAR(100) NOT NULL , `trDesc` VARCHAR(100) NOT NULL , `trDte` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`)) ENGINE = InnoDB;
 /*Transactions table edit*/
-ALTER TABLE `transactions` ADD `tr_status` INT NOT NULL DEFAULT '1' AFTER `trDte`;
+ALTER TABLE `outset`.`transactions` ADD `tr_status` INT NOT NULL DEFAULT '1' AFTER `trDte`;
 
 /*Debts Table*/
 CREATE TABLE `outset`.`debts` (`id` INT NOT NULL AUTO_INCREMENT , `debtor` VARCHAR(100) NOT NULL , `reason` VARCHAR(1000) NOT NULL , `amount` INT NOT NULL , `date_of_issue` VARCHAR(100) NOT NULL , `debt_id` VARCHAR(100) NOT NULL , `is_paid` INT NOT NULL DEFAULT '0' , `when_paid` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`)) ENGINE = InnoDB;
